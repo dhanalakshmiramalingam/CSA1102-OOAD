39c1881d3cc730925bffed9efd511cdda3678efb
dhanalakshmiramalingam <112678312+dhanalakshmiramalingam@users.noreply.github.com>
2023-08-23 09:53:21 +0530

    Add files via upload

---
+++ exp35/government.java
@@ -0,0 +1,55 @@
+
+
+/**
+ * Class government
+ */
+public class government {
+
+  //
+  // Fields
+  //
+
+  private void tax_info;
+  
+  //
+  // Constructors
+  //
+  public government () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of tax_info
+   * @param newVar the new value of tax_info
+   */
+  private void setTax_info (void newVar) {
+    tax_info = newVar;
+  }
+
+  /**
+   * Get the value of tax_info
+   * @return the value of tax_info
+   */
+  private void getTax_info () {
+    return tax_info;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void collect_tax()
+  {
+  }
+
+
+}
---
+++ exp35/land.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Class land
+ */
+public class land {
+
+  //
+  // Fields
+  //
+
+  private void land_id;
+  private void land_details;
+  private void owner;
+  private void reg_amnt;
+  private void tax_paid;
+  
+  //
+  // Constructors
+  //
+  public land () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of land_id
+   * @param newVar the new value of land_id
+   */
+  private void setLand_id (void newVar) {
+    land_id = newVar;
+  }
+
+  /**
+   * Get the value of land_id
+   * @return the value of land_id
+   */
+  private void getLand_id () {
+    return land_id;
+  }
+
+  /**
+   * Set the value of land_details
+   * @param newVar the new value of land_details
+   */
+  private void setLand_details (void newVar) {
+    land_details = newVar;
+  }
+
+  /**
+   * Get the value of land_details
+   * @return the value of land_details
+   */
+  private void getLand_details () {
+    return land_details;
+  }
+
+  /**
+   * Set the value of owner
+   * @param newVar the new value of owner
+   */
+  private void setOwner (void newVar) {
+    owner = newVar;
+  }
+
+  /**
+   * Get the value of owner
+   * @return the value of owner
+   */
+  private void getOwner () {
+    return owner;
+  }
+
+  /**
+   * Set the value of reg_amnt
+   * @param newVar the new value of reg_amnt
+   */
+  private void setReg_amnt (void newVar) {
+    reg_amnt = newVar;
+  }
+
+  /**
+   * Get the value of reg_amnt
+   * @return the value of reg_amnt
+   */
+  private void getReg_amnt () {
+    return reg_amnt;
+  }
+
+  /**
+   * Set the value of tax_paid
+   * @param newVar the new value of tax_paid
+   */
+  private void setTax_paid (void newVar) {
+    tax_paid = newVar;
+  }
+
+  /**
+   * Get the value of tax_paid
+   * @return the value of tax_paid
+   */
+  private void getTax_paid () {
+    return tax_paid;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void update_owner_details()
+  {
+  }
+
+
+  /**
+   */
+  public void update_registration()
+  {
+  }
+
+
+}
